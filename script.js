c1cc701d9fe5e8b522a132fdf41132831ca13578
Aadya <aadya_deshbhakt@outlook.com>
2025-07-22 17:54:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var menuIcon = document.querySelector(".menu-icon");
+var vidunity_logo = document.querySelector(".vidunity-logo");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("moving-sidebar");
     container.classList.toggle("large-container");
+    vidunity_logo.classList.toggle("moving-logo");
 }
 
 var play_pause = document.querySelector(".play_pause");
